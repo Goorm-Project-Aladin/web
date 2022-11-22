c2d9d4b4be61fb2c6bc502c7dfa8226850c9a2a2
britko <britko@naver.com>
2022-11-22 20:29:18 +0900

    first commit

---
+++ src/main/java/io/aladin/web/repository/BoardRepository.java
@@ -0,0 +1,12 @@
+package io.aladin.web.repository;
+
+import io.aladin.web.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Integer> {
+    Page<Board> findByTitleContaining(String searchKeyword, Pageable pageable);
+}
